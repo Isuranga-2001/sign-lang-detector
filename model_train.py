b62edeb81ba8e855b48aa50505892959f54f521f
Isuranga Warnasooriya <IsurangaWarnasooriya@outlook.com>
2025-09-17 11:17:17 +0900

    update into yolov11 to train the model for complete sign lang (v3)

--- model_train.py
+++ model_train.py
@@ -1,14 +1,14 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.pt')
+model = YOLO('yolo11n.pt')
 
 results = model.train(
-    data='datasets/asl-alphabet-v2/data.yaml',
+    data='datasets/sign-lang-v3/data.yaml',
     epochs=10,
     imgsz=640,
-    name='hand_sign_detector',
+    name='hand_sign_detector_v11',
 )
 
 val_results = model.val()
 
-model.save('hand_sign_detector.pt')
+model.save('hand_sign_detector_v11.pt')
